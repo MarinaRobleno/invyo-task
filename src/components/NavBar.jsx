c28739e05d48a9c47eb8538ea75cb88c0bf3dcec
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-28 14:05:15 +0100

    created nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@mui/material/";
+import { AiOutlineLogout } from "react-icons/ai";
 
 export function NavBar() {
-  return <div>NAV Bar</div>;
+  return (
+    <header>
+      <div className="button-line">
+        <Button className="nav-button" variant="contained">Data</Button>
+        <Button className="nav-button" variant="contained">My Tasks</Button>
+      </div>
+      <Button className="logout-button" variant="contained">
+        <AiOutlineLogout />
+      </Button>
+    </header>
+  );
 }
