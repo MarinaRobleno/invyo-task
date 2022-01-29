9e706a005fd3a2051f84991d9726bb946d4a4383
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-29 13:41:28 +0100

    created table and pagination

--- src/components/pages/MyData.jsx
+++ src/components/pages/MyData.jsx
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TableFooter,
+  TablePagination,
+} from "@mui/material";
+import data from "../../data/data";
 
 export function MyData() {
-  return <div>Data</div>;
+  const articles = data.articles;
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell style={{ fontWeight: "bold" }}>Title</TableCell>
+            <TableCell align="left" style={{ fontWeight: "bold" }}>
+              Content
+            </TableCell>
+            <TableCell align="left" style={{ fontWeight: "bold" }}>
+              Language
+            </TableCell>
+            <TableCell align="left" style={{ fontWeight: "bold" }}>
+              Tags
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0 ? articles.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : articles).map((article) => (
+            <TableRow key={articles.indexOf(article)} sx={{ height: 200 }}>
+              <TableCell component="th" scope="row">
+                {article.Title}
+              </TableCell>
+              <TableCell align="left" style={{ whiteSpace: "normal" }}>
+                {article.Content}
+              </TableCell>
+              <TableCell align="left">{article.Language}</TableCell>
+              <TableCell align="left">{article.Tags.topic}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              count={articles.length}
+              rowsPerPageOptions={[5, 10, 25]}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
 }
